1ee655849eb145ce58bbe429b850525f00fd5384
chasecook412 <chase@crossbrowsertesting.com>
2017-08-01 07:27:02 -0500

    updated latest api names

--- Lettuce-Python/features/terrain.py
+++ Lettuce-Python/features/terrain.py
@@ -13,9 +13,9 @@ def setUp():
     caps = {}
     caps['name'] = 'First Lettuce Test'
     caps['build'] = '1.0'
-    caps['browser_api_name'] = 'IE10'
-    caps['os_api_name'] = 'Win7x64-C2'
-    caps['screen_resolution'] = '1024x768'
+    caps['browser_api_name'] = 'ff-latest'              # request the latest version of firefox
+    caps['os_api_name'] = 'Win7x64'
+    caps['screen_resolution'] = '1366x768'
     caps['record_video'] = 'true'
     caps['record_network'] = 'true'
 
--- readme.md
+++ readme.md
@@ -119,8 +119,8 @@ def setUp():
     caps = {}
     caps['name'] = 'First Lettuce Test'
     caps['build'] = '1.0'
-    caps['browser_api_name'] = 'IE10'
-    caps['os_api_name'] = 'Win7x64-C2'
+    caps['browser_api_name'] = 'ff-latest'          # request the latest version of firefox
+    caps['os_api_name'] = 'Win7x64'
     caps['screen_resolution'] = '1024x768'
     caps['record_video'] = 'true'
     caps['record_network'] = 'true'
